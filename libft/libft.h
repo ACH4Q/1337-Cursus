d90013b112a1368449b53acaa661615e3034ea59
ACH4Q <achaqmohammedamine1@gmail.com>
2024-09-02 17:59:25 -0400

    generale update

--- libft/libft.h
+++ libft/libft.h
@@ -1,11 +1,15 @@
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <unistd.h>
+# include <stddef.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
 
 
 #endif
