68b52620813bfafef25d1a83e8aea2dad3e2c4ad
Mohammed Amine Achaq <machaq@e2r13p8.1337.ma>
2024-10-23 21:52:42 +0100

    test

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,23 +6,31 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:27:46 by ach4q             #+#    #+#             */
-/*   Updated: 2024/10/23 16:15:32 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/23 21:50:10 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <string.h>
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*str;
 	size_t			i;
+	unsigned char	*str;
 
+	str = (unsigned char *)b;
 	i = 0;
-	str = b;
 	while (i < len)
 	{
 		str[i] = c;
 		i++;
 	}
 	return (b);
+}
+int main()
+{
+	int i[3] = {1, 1, 1};
+	memset(i, 3, sizeof(i));
+	for (int j = 0; j < 3; j++)
+		printf("%d ", i[j]);
+	return 0;
 }
