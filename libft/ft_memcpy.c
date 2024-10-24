77c8c117a1e2675505674f9640eb2ad0502ef28a
Mohammed Amine Achaq <machaq@student.1337.ma>
2024-10-24 10:42:34 +0100

    Update libft

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:13:02 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/24 09:13:52 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/24 10:41:31 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t		i;
 	char		*dst_tmp;
 	const char	*src_tmp;
-        
-	if (dst == NULL  && src == NULL)
-		return (dst);
-	if ((src < dst) && (char *)src + n > (char *)dst)
-		return (NULL);
+    
 	dst_tmp = (char *)dst;
 	src_tmp = (const char *)src;
 	i = 0;
