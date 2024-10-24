dd088116dc95bad60058d64b40579f5f294ec432
Mohammed Amine Achaq <machaq@student.1337.ma>
2024-10-24 10:25:36 +0100

    Update libft

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,12 +6,12 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:27:46 by ach4q             #+#    #+#             */
-/*   Updated: 2024/10/24 09:04:37 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/24 10:00:42 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
+
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
