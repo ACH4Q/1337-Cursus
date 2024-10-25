56aa58ec7afce65ff5eb55eecb15bc0436389138
Mohammed Amine Achaq <machaq@student.1337.ma>
2024-10-25 14:42:10 +0100

    Update libft

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,10 +6,26 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 09:51:19 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/25 10:10:51 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/25 13:03:44 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*dest;
 
+	dest = (char *)malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 08:57:47 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/25 10:03:08 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/25 13:06:09 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
