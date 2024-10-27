1ba1d5889de08dd042caf7c6cc6f4bdce535396a
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-10-27 18:01:09 +0100

    Update libft

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:40:36 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/27 17:56:42 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/27 18:01:07 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,3 @@ char	*ft_strtrim(const char *s1, const char *set)
 	void *allocated_str = (char *)malloc(ft_strlen(s1) + 1);
 	while (s1)
 	{
-        sadsad
-    }asdasdas
-}
