a0fba7c46c1ec78a01ccc8269eb69597ba5b8a5f
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-10-27 17:34:59 +0100

    Update libft

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:15:40 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/27 17:26:26 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/27 17:32:00 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		j;
 
-    i = 0;
-    j = 0;
-	allocated_str = (char   *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!s1 && !s2)
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	allocated_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!allocated_str)
 		return (NULL);
 	while (s1[i])
 	{
@@ -36,12 +38,4 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	allocated_str[i + j] = '\0';
 	return (allocated_str);
-}
-#include <stdio.h>
-int	main(void)
-{
-    char    *str = NULL;
-    char    *str1 = NULL;
-    void *result = ft_strjoin(str,str1);
-    printf("%s",result);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 08:57:47 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/27 16:15:22 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/27 17:32:11 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,6 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char    *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
