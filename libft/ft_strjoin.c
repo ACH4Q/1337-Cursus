c6aa2f2e67ed6f9bfde280e3a54c9192feab9fe8
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-10-27 16:18:59 +0100

    Update libft

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,32 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/23 11:13:02 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/27 15:44:04 by machaq           ###   ########.fr       */
+/*   Created: 2024/10/27 16:15:40 by machaq            #+#    #+#             */
+/*   Updated: 2024/10/27 16:17:04 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-	size_t		i;
-	char		*dst_tmp;
-	const char	*src_tmp;
-
-	if (!dst && !src)
-		return (NULL);
-	dst_tmp = (char *)dst;
-	src_tmp = (const char *)src;
-	i = 0;
-	while (i < n)
-	{
-		dst_tmp[i] = src_tmp[i];
-		i++;
-	}
-	return (dst);
+    int i;
+    int j;
+    
 }
