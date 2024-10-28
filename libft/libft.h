8e3a850a38709abece411b0a2b13f313f1d5e6e6
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-10-28 20:10:27 +0100

    Update libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 08:57:47 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/27 17:39:41 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/28 20:10:20 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(const char *s1, const char *set);
 
+asdas
 #endif
