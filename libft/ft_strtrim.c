4bc04cbd30b1ea8199b550acd01c1cef64ad0d2f
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-10-28 20:09:58 +0100

    Update libft

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:40:36 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/27 18:01:07 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/28 19:12:52 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,32 @@
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
-	int i;
-	int j;
+	size_t	end;
+	size_t	start;
+	char	*str_allocated;
 
-	i = 0;
-	void *allocated_str = (char *)malloc(ft_strlen(s1) + 1);
-	while (s1)
+	start = 0;
+	end = ft_strlen(s1);
+	if (!s1 && !set)
+		return (NULL);
+	while (start < end && ft_strchr(set,s1[start]))
 	{
+		start++;
+	}
+	while (end > start && ft_strchr(set,s1[end]))
+	{
+		end--;
+	}
+	str_allocated = (char *)(malloc( (end - start  + 1)));
+	if (!str_allocated)
+		return (NULL);
+	ft_strlcpy(str_allocated,s1 + start,end - start + 1);
+	return (str_allocated);
+}
+#include <stdio.h>
+int	main()
+{
+	char *str = "ddddfffffACH4Qfdfdfdfdf";
+	char *s = "df";
+	printf("%s",ft_strtrim(str,s));
+}
