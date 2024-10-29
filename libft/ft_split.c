61453ced98c9e72ebf7641155715ce714d707ac4
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-10-29 17:49:27 +0100

    Update libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: machaq <machaq@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:35:07 by machaq            #+#    #+#             */
-/*   Updated: 2024/10/29 11:09:26 by machaq           ###   ########.fr       */
+/*   Updated: 2024/10/29 17:47:29 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char ** ft_split(char *str, char c)
+char	**ft_split(char *str, char c)
 {
-    
+	static count_word(char *str, char sep)
+	{
+		size_t count;
+        int words;
+		while (*str)
+		{
+			if (str[i] == sep && !str)
+			{
+				count++;
+                words = 1;
+			}
+            else ()
+            {
+                words = 0;
+            }
+			s++:
+		}
+		return (count);
+	}
 }
