fe269e301fdaeb94e7f4d5bd2d587f5c4efbec65
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-01 16:50:58 +0100

    Update libft

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:50:35 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/01 16:47:35 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/01 16:50:45 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	src_len;
 	size_t	dst_len;
-	
-	if (!src)
-		return (NULL);
-	if	(!dst && dstsize == 0)
+
+	if (!dst && dstsize == 0)
 		return (ft_strlen(src));
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
 	if (dstsize <= dst_len)
 	{
-		return (src_len + dstsize );	
+		return (src_len + dstsize);
 	}
 	else if (src_len < (dstsize - dst_len))
 	{
@@ -39,10 +37,4 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[dstsize - 1] = '\0';
 	}
 	return (src_len + dst_len);
-}
-int	main()
-{
-	char *str;
-	printf("%zu",ft_strlcat(NULL,"sdfds",0));
-	
 }
