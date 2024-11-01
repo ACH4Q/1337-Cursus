166402e1b93b585e430ff17c6b52039d0304939c
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-01 18:32:13 +0100

    Update libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:35:07 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/01 17:39:39 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/01 18:27:53 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,33 @@ static char	*str_alloc(const char *s, int start, int end)
 	return (str);
 }
 
+static int	fill_words(char **result,char sep,char *s)
+{
+	size_t	start;
+	size_t	i;
+	int	word;
+	
+	i = 0;
+	start = 0;
+	word = 0;
+	while (s[i])
+		{
+			if (s[i] != sep && (i == 0 || s[i - 1] == sep))
+				start = i;
+			if (s[i] != sep && (s[i + 1] == sep || s[i + 1] == '\0'))
+			{
+				result[word] = str_alloc(s, start, i + 1);
+				if (!result[word])
+					return (ft_free(result, word));
+				word++;
+			}
+		i++;
+	}
+	return (0);
+}
+
 char	**ft_split(const char *s, char sep)
 {
-	size_t	i;
-	size_t	word;
-	size_t	start;
 	size_t	words;
 	char	**result;
 
@@ -74,22 +96,9 @@ char	**ft_split(const char *s, char sep)
 	result = (char **)malloc((words + 1) * sizeof(char *));
 	if (!result)
 		return (NULL);
-	i = 0;
-	word = 0;
-	start = 0;
-	while (s[i])
-	{
-		if (s[i] != sep && (i == 0 || s[i - 1] == sep))
-			start = i;
-		if (s[i] != sep && (s[i + 1] == sep || s[i + 1] == '\0'))
-		{
-			result[word] = str_alloc(s, start, i + 1);
-			if (!result[word])
-				return (ft_free(result, word));
-			word++;
-		}
-		i++;
-	}
-	result[word] = NULL;
+	fill_words(result,sep,s);
+		if (!fill_words(result,sep,s))
+			return (NULL);
+	result[words] = NULL;
 	return (result);
 }
