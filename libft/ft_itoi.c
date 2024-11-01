6e3153692ef0148f105e797332a691da089a5e73
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-01 22:16:29 +0100

    Update libft

--- libft/ft_itoi.c
+++ libft/ft_itoi.c
@@ -6,27 +6,32 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:14:04 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/01 22:05:28 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/01 22:16:08 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static  number_len(int n)
+static  int number_len(int n)
 {
     int counter;
     if(n < 0)
         counter -= counter;
-    if (n = 0)
-        counter++;
-    if (n > 0)
+    if (n == 0)
+        counter = 1;
+    while (n != 0)
     {
         n /= n;
         counter++;
     }
     return (counter);
 }
-char    *ft_itoa(int n)
+int main()
+{
+    number_len(423432);
+}
+ /* char    *ft_itoa(int n)
 {
     if (n =)
 }
+*/
