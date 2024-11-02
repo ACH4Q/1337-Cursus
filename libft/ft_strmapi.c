ff1387a015c596dda5f7b5432c8f1c0107be8700
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-02 18:25:50 +0100

    Update libft

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,25 +6,24 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 18:17:28 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/02 18:24:00 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/02 18:25:47 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    unsigned    i;
-    char        *str;
+	unsigned i;
+	char *str;
 
-    str = malloc(ft_strlen(s) + 1);
-    if (!str)
-        return (NULL)
-    while (str)
-    {
-        str[i] = (*f)(i,s);
-        i++;
-    }
-    str[ft_strlen(s)] = '\0'
-    return (str);
+	str = malloc(ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+    while (i < ft_strlen(s))
+		{
+			str[i] = (*f)(i, s[i]);
+			i++;
+		}
+	str[ft_strlen(s)] = '\0' return (str);
 }
