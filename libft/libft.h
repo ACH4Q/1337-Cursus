a73abaefdf5442421b8d4b6812ece86288ab4501
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-03 18:04:17 +0100

    Update libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 08:57:47 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/03 17:28:40 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/03 18:04:09 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
