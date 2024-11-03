e9d9708d787e5b202a5ce6addc31d0de4bdbfcfd
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-03 17:27:10 +0100

    Update libft

--- libft/ft_lstlast_bonus.c
+++ libft/ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:21:32 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/03 17:26:47 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/03 17:27:05 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 t_list  *ft_lstlast(t_list *lst)
 {
     ft_lstsize(lst);
-    return (lst) 
+    return (lst);
 }
