d7f64971aa4bded78bb1e60842a02e78e78118ab
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-03 20:01:29 +0100

    Update libft

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 18:06:06 by machaq            #+#    #+#             */
+/*   Updated: 2024/11/03 18:50:08 by machaq           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_one;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last_one = ft_lstlast(*lst);
+	last_one->next = new;
+}
