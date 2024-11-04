4dded74eca33405cc902c4f0e72b5cb141803a71
Mohammed Amine Achaq <achaqmohammedamine1@gmail.com>
2024-11-04 13:04:26 +0100

    Update libft

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 10:49:07 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/04 10:50:35 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/04 11:13:38 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*new_obj;
+	t_list	*new_objet;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		new_obj = ft_lstnew(f(lst->content));
-		if (!new_obj)
+		new_objet = ft_lstnew(f(lst->content));
+		if (!new_objet)
 		{
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_obj);
+		ft_lstadd_back(&new_list, new_objet);
 		lst = lst->next;
 	}
 	return (new_list);
