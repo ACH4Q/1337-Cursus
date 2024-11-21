cffb3b0b203b21aaddcab407dbe45637e9416b64
ACH4Q <machaq@e2r9p8.1337.ma>
2024-11-21 18:48:30 +0100

    go home

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 14:58:14 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/21 18:42:32 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/21 18:48:27 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,21 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*allocated_str;
-	int		i;
-	int		j;
 
-	i = 0;
-	j = 0;
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
+	if (!s1)
+		return (s2);
+	if (!s2)
+		return (s1);
 	allocated_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!allocated_str)
 		return (NULL);
-	while (s1[i])
-	{
-		allocated_str[i] = s1[i];
-		i++;
-	}
-	allocated_str[i] = '\0';
-	while (s2[j])
-	{
-		allocated_str[i + j] = s2[j];
-		j++;
-	}
-	allocated_str[i + j] = '\0';
+	while (s1)
+		*allocated_str++ = *s1++;
+	free (s1)
+	while (s2)
+		*allocated_str++ = *s2++;
+	free (s2);
 	return (allocated_str);
 }
