64586e4d48967148913630a012fe5d2d32db286c
Mohammed Amine Achaq <machaq@e2r9p8.1337.ma>
2024-11-21 18:46:49 +0100

    go home

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,9 +6,36 @@
 /*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 14:58:14 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/20 14:59:58 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/21 18:42:32 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*allocated_str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	allocated_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!allocated_str)
+		return (NULL);
+	while (s1[i])
+	{
+		allocated_str[i] = s1[i];
+		i++;
+	}
+	allocated_str[i] = '\0';
+	while (s2[j])
+	{
+		allocated_str[i + j] = s2[j];
+		j++;
+	}
+	allocated_str[i + j] = '\0';
+	return (allocated_str);
+}
