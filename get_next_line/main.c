9e64a256ccc1e04dd3a6e260f3406919bca792a5
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-23 18:23:54 +0100

    Update Get_Next_Line

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -11,7 +11,7 @@ int main(void)
     fd = open("test.txt", O_RDONLY);
 
     //while ((line = get_next_line(fd)))
-    //{
+    {
         printf("%s", get_next_line(fd));  
         printf("%s", get_next_line(fd));
 
