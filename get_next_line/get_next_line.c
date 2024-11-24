7205a1b9f1ebd30510d9daf24f88ec4480a62f07
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-24 19:16:10 +0100

    Update Get_Next_Line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 18:49:09 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/24 19:14:23 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/24 19:16:00 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,12 @@ char *get_static(int fd, char *str)
 char    *get_next_line(int fd)
 {
     char        *buffer;
-    static char *str[1024];
+    static char *str;
 
     if (fd < 0|| BUFFER_SIZE <=0)
         return (NULL);
-    str[fd] = get_static(fd,str[fd]);
-    buffer = get_one_line(str[fd]);
-    str[fd] = free_static(str[fd]);
+    str = get_static(fd,str);
+    buffer = get_one_line(str);
+    str = free_static(str);
     return (buffer);
 }
