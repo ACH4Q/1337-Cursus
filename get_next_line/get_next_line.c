5594a01e0260266f9f142f2b57f3f8801cc0edfd
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-24 19:14:05 +0100

    Update Get_Next_Line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 18:49:09 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/24 19:13:59 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/24 19:14:04 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ char    *get_next_line(int fd)
     if (fd < 0|| BUFFER_SIZE <=0)
         return (NULL);
     str = get_static(fd,str);
-    buffer = get_one_line(str);
+    buffer = get_one_line(str[fd]);
     str = free_static(str[fd]);
     return (buffer);
 }
