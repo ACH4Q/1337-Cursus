b38c2896d7972eed9f694f1bf5a19c6d7ab186bf
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-30 06:17:09 +0100

    Update Get_Next_Line

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,21 +6,27 @@
 /*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 05:59:16 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/30 06:08:14 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/30 06:17:07 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int checker(char c,va_list args)
+{
+    if(c == 's')
+
+}
 int ft_printf(const char *s, ...)
 {
     va_list args;
+    va_start(args,s);
     while (s)
     {
-        if(*s =  '%')
+        if(s =  '%')
         {
             *s++;
-            
+            checker
         }
     }
     
