8545febb38e43a145e4d761dd7aeee34de2a0e6d
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-30 06:03:44 +0100

    Update Get_Next_Line

---
+++ ft_printf/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if git diff --quiet && git diff --cached --quiet; then
+    echo "No changes to push."
+    exit 0
+fi
+COMMIT_MSG=${1:-"Auto commit on $(date)"}
+git add .
+git commit -m "Update Get_Next_Line"
+git push origin main 
+echo "Changes have been pushed to the repository!"
