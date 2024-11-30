1d39553a6add5d793977e4bbd6249f36f54832f0
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-30 06:48:34 +0100

    Update ft_printf

--- ft_printf/ft_printunsnb.c
+++ ft_printf/ft_printunsnb.c
@@ -6,9 +6,11 @@
 /*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 06:43:42 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/30 06:45:40 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/30 06:48:32 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 static int num_len(unsigned int i)
 {
@@ -23,4 +25,16 @@ static int num_len(unsigned int i)
         i /= 10;
     }
     return (j);
+}
+
+int ft_printfunsnb(unsigned int i)
+{
+    if(i <= 9)
+        ft_putchar(i);
+    else
+    {
+        ft_printfunsnb(i);
+        ft_printfunsnb(i);
+    }
+    return (num_len(i));
 }
