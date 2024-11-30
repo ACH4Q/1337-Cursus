8fcfb1e7c1455b9451d245772510f596d79ffabd
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-30 06:42:19 +0100

    Update

---
+++ ft_printf/ft_printnb.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printnb.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 06:36:09 by machaq            #+#    #+#             */
+/*   Updated: 2024/11/30 06:41:55 by machaq           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int num_len(int nb)
+{
+    int i;
+    i = 0;
+    if (nb <= 0)
+        i++;
+    while (nb)
+    {
+        ++i;
+        nb /= 10;
+    }
+    return (i);
+}
+
+int ft_putnb(int nb)
+{
+	if (nb >= 0 && nb <= 9)
+		ft_putchar_fd(nb + 48);
+	else if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	else if (nb < 0)
+	{
+		ft_putchar_fd('-');
+		ft_putnb(-nb);
+	}
+	else
+	{
+		ft_putnb(nb / 10);
+		ft_putnb(nb % 10);
+	}
+    return(num_len(nb));
+}
