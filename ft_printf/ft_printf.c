e79d4031ae3bed7cd070e78ad4c5f0c12a057edc
ACH4Q <achaqmohammedamine1@gmail.com>
2024-11-30 18:13:50 +0100

    Update ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,73 +6,63 @@
 /*   By: machaq <machaq@1337.student.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 16:05:13 by machaq            #+#    #+#             */
-/*   Updated: 2024/11/30 16:05:39 by machaq           ###   ########.fr       */
+/*   Updated: 2024/11/30 18:13:27 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	checker(char const *s, va_list args, char flag)
+int	checker(char spec, va_list args, char flag)
 {
 	int	i;
 
 	i = 0;
-	if (*s == '#' || *s == '+' || *s == ' ')
-	{
-		flag = *s;
-		++s;
-	}
-	if (*s == 'c')
+	if (spec == 'c')
 		i = ft_putchar(va_arg(args, int));
-	else if (*s == 's')
+	else if (spec == 's')
 		i = ft_putstr(va_arg(args, char *));
-	else if (*s == 'd' || *s == 'i')
+	else if (spec == 'd' || spec == 'i')
 		i = ft_printnb(va_arg(args, int), flag);
-	else if (*s == 'u')
+	else if (spec == 'u')
 		i = ft_printunsnb(va_arg(args, unsigned int));
-	else if (*s == 'x')
+	else if (spec == 'x')
 		i = ft_printhex(va_arg(args, unsigned long), "0123456789abcdef", flag);
-	else if (*s == 'X')
+	else if (spec == 'X')
 		i = ft_printhex(va_arg(args, unsigned long), "0123456789ABCDEF", flag);
-	else if (*s == 'p')
+	else if (spec == 'p')
 	{
 		i = ft_putstr("0x");
 		i += ft_printhex(va_arg(args, unsigned long), "0123456789abcdef", flag);
 	}
-	else if (*s == '%')
+	else if (spec == '%')
 		i = ft_putchar('%');
 	return (i);
 }
 
 int	ft_printf(const char *s, ...)
 {
-	int		i;
 	va_list	args;
+	int		i;
 	char	flag;
 
-	i = 0;
-	flag = 0;
 	va_start(args, s);
+	i = 0;
 	while (*s)
 	{
 		if (*s == '%')
 		{
-			++s;
+			s++;
+			flag = 0;
 			if (*s == '#' || *s == '+' || *s == ' ')
 			{
 				flag = *s;
-				++s;
+				s++;
 			}
-			else
-				flag = 0;
-			i += checker(s, args, flag);
-			++s;
+			i += checker(*s, args, flag);
 		}
 		else
-		{
 			i += ft_putchar(*s);
-			++s;
-		}
+		s++;
 	}
 	va_end(args);
 	return (i);
