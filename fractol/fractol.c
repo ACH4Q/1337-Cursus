57df0d6f99d0500fbd5af7139fbdcb96870514ce
ACH4Q <achaqmohammedamine1@gmail.com>
2025-01-04 17:02:20 +0100

    Update Fractol

--- fractol/fractol.c
+++ fractol/fractol.c
@@ -6,19 +6,22 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 16:36:38 by machaq            #+#    #+#             */
-/*   Updated: 2025/01/04 16:44:07 by machaq           ###   ########.fr       */
+/*   Updated: 2025/01/04 17:02:15 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fractol.h"
+
 int main(int ac, char **av)
 {
-    if ((ac == 2 && av[1] = "mandalbrot") || (ac == 4 && av[1] = "julia"))
+    if ((ac == 2 && av[1] == "mandalbrot") || (ac == 4 && av[1] == "julia"))
     {
-        
+            t_fractol fractal;
+            fractol_init(&fractal);
     }
     else 
     {
-        write(1,"Usage ./fractol <Julia / Maldrbrot> <real> <imaginary>")
+        write(1,"Usage ./fractol <Julia / Maldrbrot> <real> <imaginary>",57);
     }
 }
 
--- fractol/fractol.h
+++ fractol/fractol.h
@@ -6,12 +6,28 @@
 /*   By: machaq <machaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 16:37:05 by machaq            #+#    #+#             */
-/*   Updated: 2025/01/04 16:37:28 by machaq           ###   ########.fr       */
+/*   Updated: 2025/01/04 17:02:06 by machaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 #define FRACTOL_H
 
+#include <unistd.h>
+
+typedef struct t_image 
+{   
+    int     line;
+    int     *image;
+    char    *pixels
+    int     bits;       
+
+} t_image;
+
+typedef struct t_mlx
+{
+    void    *windows;
+    void    *connection;
+}   t_fractal;
 
 #endif
